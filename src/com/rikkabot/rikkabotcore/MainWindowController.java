ea1b959391ed836df9945d881f78573b73a3beee
Freshek <wsrh456@gmail.com>
2017-07-12 12:55:27 +0200

    created basic gui

---
+++ src/com/rikkabot/rikkabotcore/MainWindowController.java
@@ -0,0 +1,9 @@
+package com.rikkabot.rikkabotcore;
+
+import javafx.event.ActionEvent;
+
+public class MainWindowController {
+    public void onLoginBtnPressed(ActionEvent event) {
+        System.out.println("logging in");
+    }
+}
