830f28a952cf350155ebfc56ab383d21c55a3db6
Manulaiko <manulaiko@gmail.com>
2017-07-14 17:04:33 +0200

    Improved pipeline

--- src/com/rikkabot/rikkabotcore/net/GamePipeline.java
+++ src/com/rikkabot/rikkabotcore/net/GamePipeline.java
@@ -1,18 +1,23 @@
 package com.rikkabot.rikkabotcore.net;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.ByteToMessageDecoder;
 import io.netty.handler.codec.DelimiterBasedFrameDecoder;
 import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.LengthFieldPrepender;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 import io.netty.util.CharsetUtil;
 
 import com.rikkabot.rikkabotcore.bot.GameCommandLookup;
 import com.rikkabot.rikkabotcore.bot.GameConnection;
+
+import java.util.List;
 
 /**
  * Game pipeline.
@@ -37,9 +42,49 @@ public class GamePipeline extends ChannelInitializer<SocketChannel> {
         ChannelPipeline pipeline   = ch.pipeline();
         GameConnection  connection = new GameConnection(ch);
 
-        pipeline.addLast("frameDecoder", new DelimiterBasedFrameDecoder(1024, Delimiters.nulDelimiter()))
-                .addLast("stringDecoder", new StringDecoder(CharsetUtil.UTF_8))
-                .addLast("stringEncoder", new StringEncoder(CharsetUtil.UTF_8))
-                .addLast("commandLookup", new GameCommandLookup(connection));
+        pipeline.addLast("frameDecoder", new FrameDecoder())
+                //.addLast("stringDecoder", new StringDecoder(CharsetUtil.UTF_8))
+                //.addLast("stringEncoder", new StringEncoder(CharsetUtil.UTF_8))
+                .addLast("commandLookup", new GameCommandLookup(connection))
+                .addLast("lengthPrepender", new LengthFieldPrepender(Short.BYTES));
+    }
+
+    /**
+     * Game frame decoder.
+     * ===================
+     *
+     * Decodes a received frame.
+     *
+     * @author Manulaiko <manulaiko@gmail.com>
+     */
+    public class FrameDecoder extends ByteToMessageDecoder {
+        /**
+         * Decode the from one {@link ByteBuf} to an other. This method will be called till either the input
+         * {@link ByteBuf} has nothing to read when return from this method or till nothing was read from the input
+         * {@link ByteBuf}.
+         *
+         * @param ctx the {@link ChannelHandlerContext} which this {@link ByteToMessageDecoder} belongs to
+         * @param in  the {@link ByteBuf} from which to read data
+         * @param out the {@link List} to which decoded messages should be added
+         * @throws Exception is thrown if an error accour
+         */
+        @Override
+        protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+            if (in.readableBytes() < Short.BYTES) {
+                return;
+            }
+
+            in.markReaderIndex();
+            int frameLength = in.readUnsignedShort();
+
+            if (in.readableBytes() < frameLength) {
+                in.resetReaderIndex();
+
+                return;
+            }
+
+            out.add(in.readSlice(frameLength)
+                    .retain());
+        }
     }
 }
