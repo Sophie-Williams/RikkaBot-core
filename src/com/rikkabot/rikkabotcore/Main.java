ef9856afd7b62ad558aac1183b2e474432260b6f
Freshek <wsrh456@gmail.com>
2017-07-15 11:51:38 +0200

    added cool icon

--- src/com/rikkabot/rikkabotcore/Main.java
+++ src/com/rikkabot/rikkabotcore/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,6 +15,7 @@ public class Main extends Application {
         primaryStage.setTitle("RikkaBot - 0 accounts running");
         primaryStage.setScene(new Scene(root, 600, 349));
         primaryStage.setResizable(false);
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("icon.png")));
         primaryStage.show();
     }
 
