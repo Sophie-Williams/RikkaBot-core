270f2b242044ee050880baceebf9fe10df988355
Manulaiko <manulaiko@gmail.com>
2017-07-15 12:59:49 +0200

    Created Settings class

---
+++ src/com/rikkabot/rikkabotcore/Settings.java
@@ -0,0 +1,21 @@
+package com.rikkabot.rikkabotcore;
+
+/**
+ * Settings class.
+ * ===============
+ *
+ * Application settings so they can easily be changed at runtime.
+ *
+ * @author Manulaiko <manulaiko@gmail.com>
+ */
+public class Settings {
+    /**
+     * Whether we're running in debug mode or not.
+     */
+    public static boolean debug = false;
+
+    /**
+     * Whether we should start the GUI or not.
+     */
+    public static boolean showGUI = true;
+}
