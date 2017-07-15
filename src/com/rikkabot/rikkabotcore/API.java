b20a1fa1417c89995cde001549bbf40578b27823
Manulaiko <manulaiko@gmail.com>
2017-07-15 18:38:26 +0200

    Created API interface

---
+++ src/com/rikkabot/rikkabotcore/API.java
@@ -0,0 +1,12 @@
+package com.rikkabot.rikkabotcore;
+
+/**
+ * API interface.
+ * ==============
+ *
+ * API to communicate the plugins with the core.
+ *
+ * @author Manulaiko <manulaiko@gmail.com>
+ */
+public interface API {
+}
