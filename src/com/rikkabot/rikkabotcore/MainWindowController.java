135b82bad200c52b7410b93b7e034843c6bedd42
Manulaiko <manulaiko@gmail.com>
2017-07-15 13:17:58 +0200

    Minor verbose improvements.

--- src/com/rikkabot/rikkabotcore/MainWindowController.java
+++ src/com/rikkabot/rikkabotcore/MainWindowController.java
@@ -5,14 +5,18 @@ import com.rikkabot.rikkabotcore.dao.hero.Hero;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 
+import com.manulaiko.tabitha.Console;
+
 public class MainWindowController {
 
     public TextField usernameField;
     public PasswordField passwordField;
 
     public void onLoginBtnPressed(ActionEvent event) {
-        System.out.println("logging in");
+        Console.debug("Logging in...");
 
         Hero hero = FactoryManager.heroes.login(usernameField.getText(), passwordField.getText());
+
+        Console.debug("Successfully logged in!");
     }
 }
--- src/com/rikkabot/rikkabotcore/net/HttpClient.java
+++ src/com/rikkabot/rikkabotcore/net/HttpClient.java
@@ -9,9 +9,13 @@ import java.io.InputStreamReader;
 import java.net.*;
 import java.net.HttpURLConnection;
 
+import lombok.Data;
+import lombok.experimental.Accessors;
+
 /**
  * Created by piotr on 12.07.17.
  */
+@Accessors @Data
 public class HttpClient {
     private URL locationUrl;
 
@@ -26,7 +30,8 @@ public class HttpClient {
      */
     public String get(String targetUrl) {
         try {
-            System.out.println(targetUrl);
+            Console.debug("Executing GET to "+ targetUrl);
+
             URL url = new URL(targetUrl);
 
             HttpURLConnection connection = (HttpURLConnection)url.openConnection();
@@ -36,7 +41,6 @@ public class HttpClient {
             connection.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0");
 
             return readHttpResponse(connection);
-
         } catch (Exception e) {
             Console.print(e);
             return null;
@@ -71,6 +75,7 @@ public class HttpClient {
             return readHttpResponse(connection);
         } catch (Exception e) {
             Console.print(e);
+
             return null;
         }
     }
@@ -90,8 +95,10 @@ public class HttpClient {
                 String location = connection.getHeaderField("Location");
                 if (location.startsWith("/")) {
                     URI uri = new URI(connection.getURL().toString());
+
                     return get(uri.getScheme() + "://" + uri.getHost() + location);
                 }
+
                 return get(connection.getHeaderField("Location"));
             }
 
@@ -104,9 +111,11 @@ public class HttpClient {
             reader.close();
 
             locationUrl = connection.getURL();
+
             return ret.toString();
         } catch (Exception e) {
             Console.print(e);
+
             return null;
         }
     }
@@ -119,14 +128,12 @@ public class HttpClient {
     private String prepareHttpPostData(String postData) {
         try {
             URI uri = new URI(postData);
+
             return uri.toASCIIString();
         } catch (Exception e) {
             Console.print(e);
+
             return null;
         }
-    }
-
-    public URL locationUrl() {
-        return this.locationUrl;
     }
 }
